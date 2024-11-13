dc863aed487e67402a509c4709d196cffa1b35e0
Steven Nguyen <zerga556@yahoo.com>
2024-11-12 23:00:37 -0600

    Speak button implemented
    Created a speak button that will play audio and do a live audio of the translation when toggled on

---
+++ src/app/components/SpeakButton.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+interface SpeakButtonProps {
+  onClick: () => void
+  label?: string
+}
+
+const SpeakButton: React.FC<SpeakButtonProps> = ({
+  onClick,
+  label = "Play Translated Audio",
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-full p-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+      {label}
+    </button>
+  )
+}
+
+export default SpeakButton
