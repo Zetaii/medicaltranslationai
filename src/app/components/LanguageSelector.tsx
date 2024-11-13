8447d37d52e83cd29aa6d5d685bfa86d56a5c5d1
Steven Nguyen <zerga556@yahoo.com>
2024-11-12 23:26:57 -0600

    Updated some styling for mobile responsiveness
    Set text to text-base on small devices.

--- src/app/components/LanguageSelector.tsx
+++ src/app/components/LanguageSelector.tsx
@@ -15,11 +15,13 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = ({
 }) => {
   return (
     <div className="space-y-1">
-      <label className="text-white font-semibold">{label}</label>
+      <label className="text-white font-semibold text-sm sm:text-base">
+        {label}
+      </label>
       <select
         value={value}
         onChange={onChange}
-        className="w-full p-2 border text-black border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full p-2 text-sm sm:text-base border text-black border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
